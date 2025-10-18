842b6032eef9ea3d6f760d40f3b63e8512c34a91
Melisa Martellini <melisainesmartellini@gmail.com>
2025-10-18 10:49:58 -0300

    Add files via upload

--- TP1_Biblioteca.sql
+++ TP1_Biblioteca.sql
@@ -3,7 +3,7 @@ CREATE DATABASE Biblioteca
 use Biblioteca
 
 create table FichaBibliografica (
-id_ficha int PRIMARY KEY,
+id_ficha int PRIMARY KEY AUTO_INCREMENT ,
 eliminado BOOLEAN,
 isbn VARCHAR(17) UNIQUE,
 clasificacionDewey VARCHAR(20),
@@ -12,7 +12,7 @@ idioma VARCHAR(30)
 );
 
 create table Libro (
-id_Libro int PRIMARY KEY, 
+id_Libro int PRIMARY KEY AUTO_INCREMENT, 
 eliminado BOOLEAN,
 titulo VARCHAR(150) NOT NULL,
 autor VARCHAR(120) NOT NULL,
@@ -21,4 +21,87 @@ anioEdicion INT,
 id_ficha INT UNIQUE,
 FOREIGN KEY (id_ficha) REFERENCES FichaBibliografica(id_ficha)
 );
+
+
+USE Biblioteca;
+
+CREATE TABLE Idioma (
+    id_idioma INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_idioma VARCHAR(30) UNIQUE NOT NULL
+);
+
+USE Biblioteca;
+INSERT INTO Idioma (nombre_idioma) VALUES
+('Español'),
+('Inglés'),
+('Francés'),
+('Alemán');
+USE Biblioteca;
+
+ALTER TABLE FichaBibliografica DROP COLUMN idioma;
+USE Biblioteca;
+
+ALTER TABLE FichaBibliografica ADD COLUMN id_idioma INT NOT NULL;
+USE Biblioteca;
+
+ALTER TABLE FichaBibliografica
+ADD CONSTRAINT fk_idioma
+FOREIGN KEY (id_idioma)
+REFERENCES Idioma(id_idioma);
+
+USE Biblioteca;
+SHOW TABLES;
+
+USE Biblioteca;
+
+CREATE TABLE Numeros (
+  n INT PRIMARY KEY
+);
+
+
+
+INSERT INTO FichaBibliografica (eliminado, isbn, clasificacionDewey, estanteria, id_idioma)
+SELECT 
+  FALSE,
+  CONCAT('978-', LPAD(n, 12, '0')),
+  CONCAT(FLOOR(RAND() * 999), '.', FLOOR(RAND() * 99)),
+  CONCAT('EST-', MOD(n, 100)),
+  1 + MOD(n, 4)
+FROM Numeros;
+
+INSERT INTO Libro (eliminado, titulo, autor, editorial, anioEdicion, id_ficha)
+SELECT 
+  FALSE,
+  CONCAT('Título ', id_ficha),
+  CONCAT('Autor ', CHAR(65 + MOD(id_ficha, 26)), ' ', CHAR(65 + MOD(id_ficha DIV 26, 26))),
+  CONCAT('Editorial ', MOD(id_ficha, 50)),
+  1900 + MOD(id_ficha, 125),
+  id_ficha
+FROM FichaBibliografica;
+
+
+
+SELECT COUNT(*) FROM FichaBibliografica;
+
+SELECT COUNT(*) FROM Libro;
+
+DROP TABLE Numeros;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
